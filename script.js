195f9d73f184240d4f958f81aff5b89da6233242
Hiprithvi <prajsharma25@gmail.com>
2026-02-12 23:26:55 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,10 @@
-// Fade reveal
-const fades = document.querySelectorAll('.fade');
+const bars = document.querySelectorAll('.bar');
 
 window.addEventListener('scroll', () => {
-    fades.forEach(section => {
-        const top = section.getBoundingClientRect().top;
-        if(top < window.innerHeight - 100){
-            section.classList.add('show');
-        }
-    });
-
-    // Progress animation
-    const bars = document.querySelectorAll('.bar');
-    bars.forEach(bar => {
-        const top = bar.getBoundingClientRect().top;
-        if(top < window.innerHeight - 50){
-            bar.style.width = bar.getAttribute('data-width');
-        }
-    });
+  bars.forEach(bar => {
+    const top = bar.getBoundingClientRect().top;
+    if(top < window.innerHeight - 50){
+      bar.style.width = bar.getAttribute('data-width');
+    }
+  });
 });

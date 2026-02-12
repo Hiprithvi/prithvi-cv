0b4f774510906796d6a818b1b37f4d92c7c62291
Hiprithvi <prajsharma25@gmail.com>
2026-02-12 22:07:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 
+const fades = document.querySelectorAll('.fade');
+
+window.addEventListener('scroll', () => {
+    fades.forEach(section => {
+        const sectionTop = section.getBoundingClientRect().top;
+        if (sectionTop < window.innerHeight - 100) {
+            section.classList.add('show');
+        }
+    });
+});
+
+// Typing effect
+const text = "Entry-Level Professional | Editing & Computer Skills";
+let i = 0;
+function typing() {
+    if (i < text.length) {
+        document.querySelector(".typing").innerHTML += text.charAt(i);
+        i++;
+        setTimeout(typing, 50);
+    }
+}
+typing();
